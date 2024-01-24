931c654da964bd706c998c852e089825099a20e2
Donato Valenzano <valenzd@sunypoly.edu>
2024-01-24 13:41:53 -0500

    Screen Stuff Beginning towers etc

--- tester.py
+++ tester.py
@@ -4,7 +4,21 @@ import pygame
 pygame.init()
 
 # CREATING CANVAS
-canvas = pygame.display.set_mode((500, 500))
+canvas = pygame.display.set_mode((800, 500))
+
+WHITE = (255, 255, 255)
+# LEFT TOWER
+tower1 = pygame.Rect(30, 200, 70, 70)
+pygame.draw.rect(canvas, WHITE, tower1, 2)
+# RIGHT TOWER
+tower2 = pygame.Rect(700, 200, 70, 70)
+pygame.draw.rect(canvas, WHITE, tower2, 2)
+# LEFT BARRIER LINE
+pygame.draw.line(canvas, WHITE, (150,0), (150,500), 1)
+# RIGHT BARRIER LINE
+pygame.draw.line(canvas, WHITE, (650,0), (650,500), 1)
+
+
 
 # TITLE OF CANVAS
 pygame.display.set_caption("My Board")
