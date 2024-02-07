87aa2554d6c9765a5a09407a57bb89a5fc81145a
Isabella Cornish <cornisi@sunypoly.edu>
2024-02-07 12:29:53 -0500

    Fight mechanics

--- oneVone.py
+++ oneVone.py
@@ -37,26 +37,71 @@ def defeat(fighter):
         god_list.remove(fighter)
 
 
-screen = pygame.display.set_mode((800, 500))
+# screen = pygame.display.set_mode((800, 500))
 
 player = TrojanHorse()
 player.rect.x = 0
 player.rect.y = 200
 
+player2 = Achilles()
+player2.rect.x = -150
+player2.rect.y = 200
+
+player3 = FootSoldier()
+player3.rect.x = -300
+player3.rect.y = 200
+
+player4 = Eagle()
+player4.rect.x = -450
+player4.rect.y = 200
+
+player5 = Archer()
+player5.rect.x = 0
+player5.rect.y = 200
+
+player6 = Cavalry()
+player6.rect.x = -600
+player6.rect.y = 200
+
 enemy = Hellhound()
 enemy.rect.x = 750
 enemy.rect.y = 200
 
-enemy2 = Hellhound()
+enemy2 = Minion()
 enemy2.rect.x = 900
 enemy2.rect.y = 200
+
+enemy3 = Medusa()
+enemy3.rect.x = 1050
+enemy3.rect.y = 200
+
+enemy4 = Harpy()
+enemy4.rect.x = 1200
+enemy4.rect.y = 200
+
+enemy5 = Cyclops()
+enemy5.rect.x = 1350
+enemy5.rect.y = 200
+
+enemy6 = Sorceress()
+enemy6.rect.x = screen.get_width()-enemy6.width
+enemy6.rect.y = 200
 
 mortal_list = pygame.sprite.Group()
 god_list = pygame.sprite.Group()
 
 mortal_list.add(player)
+mortal_list.add(player2)
+mortal_list.add(player3)
+mortal_list.add(player4)
+mortal_list.add(player5)
+mortal_list.add(player6)
 god_list.add(enemy)
 god_list.add(enemy2)
+god_list.add(enemy3)
+god_list.add(enemy4)
+god_list.add(enemy5)
+god_list.add(enemy6)
 
 running = True
 clock = pygame.time.Clock()
@@ -82,16 +127,16 @@ while running:
             god.move_left(god.speed)
 
     # updates
-    mortal_list.update()
-    god_list.update()
     mortal_list.draw(screen)
     god_list.draw(screen)
 
     # update health label (after .draw() so that it isn't overwritten
     for mortal in mortal_list:
         mortal.update_health_label()
+        mortal.update()
     for god in god_list:
         god.update_health_label()
+        god.update()
 
     pygame.display.update()
 
